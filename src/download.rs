6680f6beb6c8d5f031e0eb264f0f8a541fac4126
matteobolner <matteo.bolner2@unibo.it>
2024-09-13 13:14:02 +0200

    Download works

--- src/download.rs
+++ src/download.rs
@@ -1,10 +1,10 @@
-use reqwest;
+use reqwest::get;
 use std::io::{self, Write};
 
 #[tokio::main]
 pub async fn download(url: String) -> Result<(), Box<dyn std::error::Error>> {
     let mut stdout = io::stdout().lock();
-    let body = reqwest::get(&url).await?.text().await?;
+    let body = get(&url).await?.text().await?;
     stdout.write_all(body.as_bytes())?;
     stdout.flush()?;
     Ok(())
