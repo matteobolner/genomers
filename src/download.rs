5a330882b2a61d243c58d5e94dfb07d4dfc37503
matteobolner <matteo.bolner2@unibo.it>
2024-09-15 15:15:00 +0200

    Converted download to streamed download

--- src/download.rs
+++ src/download.rs
@@ -1,3 +1,4 @@
+use futures_util::StreamExt;
 use reqwest::get;
 use std::io::{self, Write};
 
@@ -8,8 +9,12 @@ pub async fn download(url: String) -> Result<(), Box<dyn std::error::Error>> {
     if resp.status() != reqwest::StatusCode::OK {
         return Err(format!("Invalid URL: {}", url).into());
     }
-    let body = resp.bytes().await?;
-    stdout.write_all(&body)?;
+    // let body = resp.bytes().await?;
+    let mut stream = resp.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        stdout.write_all(&item.expect("ERROR DOWNLOADING"))?;
+    }
     stdout.flush()?;
     Ok(())
 }
